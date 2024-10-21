1cf52dae359032a8e8616d6b783320a08f2d6351
Ice-Sigil <grayjack04@hotmail.com>
2024-10-21 16:35:42 -0400

    modified:   Deep Learning Python/CNN.py

--- Deep Learning Python/CNN.py
+++ Deep Learning Python/CNN.py
@@ -1,6 +1,6 @@
 #Attempting to run this on your own system requires changing the directory paths to match your own system.
 #Also, make sure your directories are set up correctly for datagen.flow_from_directory!
-#Run test(3) first, and then test (2) to make sure your directories are set up.
+#Run test(3) first, and then test (2) to make sure your directories are set up, and then 2.5 to finalize file structure.
 import sys
 import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2" #This does something important probably
@@ -22,30 +22,32 @@ labels = df['dx'].values
 #This section is very important and is how we import our images! Don't mess with it
 from keras.src.legacy.preprocessing.image import ImageDataGenerator
 datagen = ImageDataGenerator()
-train_data_keras = datagen.flow_from_directory(directory = 'C:/Users/grayj/Desktop/DATASET/Train', class_mode = 'categorical', batch_size = 2, target_size=(32,32), shuffle = False)
+train_data_keras = datagen.flow_from_directory(directory = 'C:/Users/grayj/Desktop/DATASET/Train', class_mode = 'categorical', batch_size = 16, target_size=(32,32), shuffle = False)
 x, y = next(train_data_keras)
+#print(train_data_keras.filenames)
 
+'''
 #This section will be deleted, as it just shows the first 16 images the above section gets
 for i in range(0,15):
     image = x[i].astype(int)
     plt.imshow(image)
     plt.show()
 '''
-#This is our actual model. I don't know how it really works but don't mess with it
+
+#This is our actual model.
 model = models.Sequential()
-model.add(layers.Conv2D(28, (3, 3), activation='relu', input_shape=(28,28,3)))
+model.add(layers.Conv2D(16, kernel_size = (3,3), activation = 'relu', padding = 'same'))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(7))
-model.summary()
+model.add(layers.Dense(2))
 
-#This does something
+#This does something important don't mess with it 
 model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
 #This is our new input, and will be how we feed into model.fit, the command that actually trains the model.
@@ -55,5 +57,10 @@ test_csv = pd.read_csv('C:/Users/grayj/Desktop/DATASET/test.csv', names = ["dx"]
 train_csv = pd.read_csv('C:/Users/grayj/Desktop/DATASET/train.csv', names = ["dx"])
 valid_csv = pd.read_csv('C:/Users/grayj/Desktop/DATASET/valid.csv', names = ["dx"])
 
-model.fit(train_data_keras, train_csv, header=None, names='dx')
-'''
+#The command that trains the model
+model.fit(train_data_keras, epochs=5, validation_data=valid_data_keras)
+
+#This should set the model to predict on the testing data- however it gives no result?
+model.predict(test_data_keras, verbose=1)
+
+model.summary()
