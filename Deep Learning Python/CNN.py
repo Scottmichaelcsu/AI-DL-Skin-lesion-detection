e1e76b6a030415c90415b39b432ac3e7896cb889
Ice-Sigil <grayjack04@hotmail.com>
2024-10-28 17:06:14 -0400

    Attemtping new ways of plotting the output of our model. 	modified:   Deep Learning Python/CNN.py

--- Deep Learning Python/CNN.py
+++ Deep Learning Python/CNN.py
@@ -13,19 +13,20 @@ import numpy as np
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
 
-
+'''
 #This whole section is likely outdated and will be replaced later.
 directory1 = 'C:/Users/grayj/Desktop/DATASET'
 df= pd.read_csv('C:/Users/grayj/Desktop/DATASET/HAM10000_metadata.csv')
 file_paths = df['lesion_id'].values 
 labels = df['dx'].values
 #This whole section is likely outdated and will be replaced later.
+'''
 
 #This section is very important and is how we import our images! Don't mess with it
 from keras.src.legacy.preprocessing.image import ImageDataGenerator
 datagen = ImageDataGenerator()
 train_data_keras = datagen.flow_from_directory(directory = 'C:/Users/grayj/Desktop/DATASET/Train', class_mode = 'categorical', batch_size = 16, target_size=(32,32), shuffle = False)
-x, y = next(train_data_keras)
+#x, y = next(train_data_keras)
 
 
 #This is our actual model.
@@ -56,10 +57,66 @@ history = model.fit(train_data_keras, epochs=1, validation_data=valid_data_keras
 
 #This should set the model to predict on the testing data- however it gives no result?
 score = model.evaluate(test_data_keras)
+print(score)
 
 
-#I tried to chart the data using plotlib according to that one guy's videos on HAM10000
-#So far didn't work, but I might try another method.
+#No output from this when plotted- I suspect it's because of history.history. 
+#I can't find much information on that command, and what information I did find was outdated.
+#It's likely been completely replaced.
+# https://www.tensorflow.org/tensorboard/graphs ?
+'''
+print(f"Test loss: {score[0]}%")
+print(f"Test accuracy: {score[1]*100}%")
 
+# Plotting accuracy
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+plt.plot(history.history['accuracy'], label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(loc='lower right')
 
-#model.summary()
+# Plotting loss
+plt.subplot(1, 2, 2)
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.title('Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(loc='upper right')
+
+plt.tight_layout()
+plt.show()
+
+# Function to display images with predictions
+def display_images_with_predictions(datagen, model, num_images=5):
+    plt.figure(figsize=(15, 15))
+    for i in range(num_images):
+        # Get a batch of test data
+        x, y = next(datagen)
+        
+        # Make predictions
+        predictions = model.predict(x)
+        
+        # Convert predictions to class labels
+        predicted_classes = np.argmax(predictions, axis=1)
+        
+        for j in range(len(x)):
+            plt.subplot(num_images, 2, 2 * i + 1)
+            plt.imshow(x[j])  # Display image
+            plt.title(f'Predicted: {"Malignant" if predicted_classes[j] == 1 else "Benign"}')
+            plt.axis('off')
+
+            plt.subplot(num_images, 2, 2 * i + 2)
+            plt.imshow(x[j])  # Display image again (or use any other example)
+            plt.title(f'Actual: {"Malignant" if np.argmax(y[j]) == 1 else "Benign"}')
+            plt.axis('off')
+            
+    plt.tight_layout()
+    plt.show()
+
+# Call the function
+display_images_with_predictions(test_data_keras, model)
+'''
