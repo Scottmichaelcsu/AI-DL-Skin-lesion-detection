c930cb6a19ca9992b2e5295ddcae0a995086aaf2
Ice-Sigil <grayjack04@hotmail.com>
2024-11-23 21:14:13 -0500

    Final for real modified:   Deep Learning Python/CNN.py 	modified:   README.md

--- Deep Learning Python/CNN.py
+++ Deep Learning Python/CNN.py
@@ -10,6 +10,43 @@ from keras import layers, models
 from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
+import tensorflow.keras.backend as K
+
+def recall(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall_keras = true_positives / (possible_positives + K.epsilon())
+    return recall_keras
+
+
+def precision(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision_keras = true_positives / (predicted_positives + K.epsilon())
+    return precision_keras
+
+
+def specificity(y_true, y_pred):
+    tn = K.sum(K.round(K.clip((1 - y_true) * (1 - y_pred), 0, 1)))
+    fp = K.sum(K.round(K.clip((1 - y_true) * y_pred, 0, 1)))
+    return tn / (tn + fp + K.epsilon())
+
+
+def f1(y_true, y_pred):
+    p = precision(y_true, y_pred)
+    r = recall(y_true, y_pred)
+    return 2 * ((p * r) / (p + r + K.epsilon()))
+
+
+def matthews_correlation_coefficient(y_true, y_pred):
+    tp = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    tn = K.sum(K.round(K.clip((1 - y_true) * (1 - y_pred), 0, 1)))
+    fp = K.sum(K.round(K.clip((1 - y_true) * y_pred, 0, 1)))
+    fn = K.sum(K.round(K.clip(y_true * (1 - y_pred), 0, 1)))
+
+    num = tp * tn - fp * fn
+    den = (tp + fp) * (tp + fn) * (tn + fp) * (tn + fn)
+    return num / K.sqrt(den + K.epsilon())
 
 #This section is very important and is how we import our images! Don't mess with it
 from keras.src.legacy.preprocessing.image import ImageDataGenerator
@@ -30,7 +67,7 @@ model.add(layers.Dense(2))
 #This prepares the model for running and specifies what metrics we are aiming for
 model.compile(optimizer='adam',
               loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+              metrics=['accuracy', precision, recall, f1, specificity, matthews_correlation_coefficient])
 
 #This is our new input, and will be how we feed into model.fit, the command that actually trains the model.
 test_data_keras = datagen.flow_from_directory(directory = 'C:/Users/grayj/Desktop/DATASET/Test', class_mode = 'categorical', batch_size = 16, target_size=(32,32))
@@ -38,7 +75,7 @@ valid_data_keras = datagen.flow_from_directory(directory = 'C:/Users/grayj/Deskt
 log_dir1 = 'C:/Users/grayj/Desktop/DATASET/logs'
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir1, histogram_freq=1)
 #The command that trains the model
-history = model.fit(train_data_keras, epochs=1, validation_data=valid_data_keras, callbacks= [tensorboard_callback])
+history = model.fit(train_data_keras, epochs=4, validation_data=valid_data_keras, callbacks= [tensorboard_callback])
 
 #This sets the model to evaluate itself on the testing data and write to Tensorboard
-model.evaluate(test_data_keras, callbacks = [tensorboard_callback])
+model.predict(test_data_keras, callbacks = [tensorboard_callback])
--- README.md
+++ README.md
@@ -2,18 +2,41 @@
 # README 1.4 09/24/2024
 Software Engineering 2024 Fall semester AI-DL group project.
 
-TEAM: Michael Scott, Chance Daniel, Gray Jackson, Carnell Greenfield
+Project Title
 
-TEAM NAME: "WORK IN PROGRESS"
+A simple Tensorflow CNN that can detect the presence of malignant cancer in skin lesions.
+Description
+    This is a python script that uses Keras and Tensorflow's Convolutional Neural Network systems to read in a set of training data, learn from it, and predict whether new data is cancerous or benign based off its knowledge.
 
-PROJECT: Deep Learning AI Skin Cancer Detection.
+Getting Started
+Dependencies
 
-PROJ REQ: given an image the AI needs to detect if skin cancer is present.
+    Tensorflow
+    Anaconda
+    Tensorboard Visual Studio Code Extension
+Installing
 
-Dataset: https://www.kaggle.com/datasets/kmader/skin-cancer-mnist-ham10000
+    Change all directories in test(2.5).py and CNN.py to match your current system's directories.
 
-STATUS: setting up correct format/orginization for model.fit and prep for presentation.
+Executing program
 
-Current Files [3]: two for preprocessing and the main CNN file. 
+To run this, you must first download the HAM10000 dataset and have it locally on your computer.
+From there, open test(2.5).py and change all the directories specified to match on your system.
+Run test(2.5).py. This will create directories and sort your dataset into the format required by the model.
+From there, open CNN.py and change the directories according to how it is set up on your PC. 
+Have Tensorflow and Anaconda installed, and then run the model.
+To access Tensorboard, ensure you have the extension installed in Visual Studio code, and then hit CTRL+Shift+P.
+You should see the command bar open up and from there, hit "Python: Launch Tensorboard". 
 
-TASK: Confirm preprocessing complete and formatting correct, soon we unleash the AI.
+
+Help
+Ensure all backslashes (\) are replaced with frontslashes (/) in all directories.
+Lower batch size if running out of memory.
+
+
+Authors
+
+Jackson Gray
+Chance Daniel
+Michael Scott
+Carnell Greenfield
